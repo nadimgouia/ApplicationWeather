9c0d6e0940f1b5cd8d16cbac49c678053e89566e
Nadim GOUIA <ngouia@telecomdesign.fr>
2021-12-04 17:17:44 +0100

    part 2, use gson converterFactory

--- app/src/main/java/com/ndroid/weatherapp2/MainActivity.kt
+++ app/src/main/java/com/ndroid/weatherapp2/MainActivity.kt
@@ -2,29 +2,45 @@ package com.ndroid.weatherapp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import com.google.gson.JsonObject
+import com.squareup.picasso.Picasso
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var tvResponse: TextView
+    lateinit var editCityName: EditText
+    lateinit var btnSearch: Button
+    lateinit var imageWeather: ImageView
+    lateinit var tvTemperature: TextView
+    lateinit var tvCityName: TextView
+
+    lateinit var layoutWeather: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvResponse = findViewById(R.id.tvResponse)
+        editCityName = findViewById(R.id.editCity)
+        btnSearch = findViewById(R.id.btnSearch)
+        imageWeather = findViewById(R.id.imageWeather)
+        tvCityName = findViewById(R.id.tvCityName)
+        tvTemperature = findViewById(R.id.tvTemperature)
+        layoutWeather = findViewById(R.id.layoutWeather)
+
+
 
         // TODO1: create retrofit instance
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.openweathermap.org/data/2.5/weather/")
-            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         val weatherService = retrofit.create(WeatherService::class.java)
@@ -32,14 +48,28 @@ class MainActivity : AppCompatActivity() {
         // TODO2: call weather api
         val result = weatherService.getWeatherByCity()
 
-        result.enqueue(object : Callback<String> {
-            override fun onResponse(call: Call<String>, response: Response<String>) {
+        result.enqueue(object : Callback<JsonObject> {
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
                 if(response.isSuccessful) {
-                    tvResponse.text = response.body()
+                    val result = response.body()
+                    val main = result?.get("main")?.asJsonObject
+                    val temp = main?.get("temp")?.asDouble
+                    val cityName = result?.get("name")?.asString
+
+                    val weather = result?.get("weather")?.asJsonArray
+                    val icon = weather?.get(0)?.asJsonObject?.get("icon")?.asString
+
+                    Picasso.get().load("https://openweathermap.org/img/w/$icon.png").into(imageWeather)
+
+                    tvTemperature.text = "$temp °C"
+                    tvCityName.text = cityName
+
+                    layoutWeather.visibility = View.VISIBLE
+
                 }
             }
 
-            override fun onFailure(call: Call<String>, t: Throwable) {
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
                Toast.makeText(applicationContext, "Erreur serveur", Toast.LENGTH_SHORT).show()
             }
 
--- app/src/main/java/com/ndroid/weatherapp2/WeatherService.kt
+++ app/src/main/java/com/ndroid/weatherapp2/WeatherService.kt
@@ -1,10 +1,11 @@
 package com.ndroid.weatherapp2
 
+import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface WeatherService {
 
     @GET("?q=Paris&appid=915a276fe2f42f1255762165dd49adce&units=metric")
-    fun getWeatherByCity(): Call<String>
+    fun getWeatherByCity(): Call<JsonObject>
 }
